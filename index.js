c6ec606e512ba6b11da82350ab4a4137bee60503
Bob Reselman <reselbob@gmail.com>
2020-09-26 15:47:15 -0700

    intial commit

--- index.js
+++ index.js
@@ -3,25 +3,44 @@
 const http = require('http');
 const port = process.env.APP_PORT || 3000;
 
-/** Starts a HTTP server that receives requests on sample server port. */
-function startServer(port) {
-  // Creates a server
-  const server = http.createServer(handleRequest);
-  // Starts the server
-  server.listen(port, (err) => {
-    if (err) {
-      throw err;
+const shutdown = (signal) => {
+    if (!signal) {
+        console.log(`API Server shutting down at ${new Date()}`);
+    } else {
+        console.log(`Signal ${signal} API Server shutting down at ${new Date()}`);
     }
-    console.log(`Node HTTP listening on ${port}`);
-  });
+    server.close(function () {
+        process.exit(0);
+    })
+};
+
+// Creates a server
+const server = http.createServer(handleRequest);
+// Starts the server
+server.listen(port, (err) => {
+if (err) {
+    throw err;
 }
+console.log(`Node HTTP listening on ${port}`);
+});
 
 /** A function which handles requests and send response. */
 function handleRequest(request, response) {
-    const str = process.env.SIMPLE_TRAVIS_MESSAGE || `Hello at ${new Date()}`
+    const message = process.env.SIMPLE_TRAVIS_MESSAGE || `Hello at ${new Date()}`
     response.setHeader("Content-Type", "application/json");
     response.writeHead(200);
-    response.end(str);
+    response.end(JSON.stringify({message}));
 }
 
-startServer(port);
+process.on('SIGTERM', function () {
+    shutdown('SIGTERM');
+});
+
+process.on('SIGINT', function () {
+    shutdown('SIGINT');
+});
+
+module.exports = { server, shutdown };
+
+
+

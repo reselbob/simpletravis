646b43bc5049f1fc1be8f12f286c67690abc4b1e
Bob Reselman <reselbob@gmail.com>
2020-09-26 21:20:45 -0700

    updating travis.yml

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ console.log(`Node HTTP listening on ${port}`);
 
 /** A function which handles requests and send response. */
 function handleRequest(request, response) {
-    let message = `Hello at ${new Date()}`
-    if(process.env.SIMPLE_TRAVIS_MESSAGE) message = 'I cannot tell you.'
+    const message = process.env.SIMPLE_TRAVIS_MESSAGE || `Hello at ${new Date()}`
     response.setHeader("Content-Type", "application/json");
     response.writeHead(200);
     response.end(JSON.stringify({message}));
